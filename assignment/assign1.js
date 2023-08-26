20a0cdd074b6f7fb1fa01a54f998ee54af3a5e1a
Prinson <20cs113.prinson@sjec.ac.in>
2023-08-25 18:56:22 -0700

    third commit

--- assignment/assign1.js
+++ assignment/assign1.js
@@ -1,6 +1,7 @@
-// writ
-var numbers = [1,2,3,-4,-4,-6];
+// write a javascript function that takes an array of numbers and returns the sum of all
+//  positive numbers in the array
 
+var numbers = [1,2,3,-4,-4,-6];
 var sum=0;
 for(var i=0;i<numbers.length;i++){
     if(numbers[i]>0){
